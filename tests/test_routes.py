6f5e90389a6bff0aaeb4ba66e021a5a7674e7b23
w-garibaldi <137449605+w-garibaldi@users.noreply.github.com>
2023-06-25 09:40:39 -0500

    Add files via upload

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -22,9 +22,11 @@ BASE_URL = "/accounts"
 
 HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
+
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
+
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
@@ -176,24 +178,23 @@ class TestAccountService(TestCase):
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
-
 ######################################################################
 #  SECURITY HEADER TEST
 ######################################################################
-    def test_security_headers(self):
-            """It should return security headers"""
-            response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
-            self.assertEqual(response.status_code, status.HTTP_200_OK)
-            headers = {
-                'X-Frame-Options': 'SAMEORIGIN',
-                'X-XSS-Protection': '1; mode=block',
-                'X-Content-Type-Options': 'nosniff',
-                'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
-                'Referrer-Policy': 'strict-origin-when-cross-origin'
-            }
-            for key, value in headers.items():
-                self.assertEqual(response.headers.get(key), value)
 
+    def test_security_headers(self):
+        """It should return security headers"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        headers = {
+            'X-Frame-Options': 'SAMEORIGIN',
+            'X-XSS-Protection': '1; mode=block',
+            'X-Content-Type-Options': 'nosniff',
+            'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
+            'Referrer-Policy': 'strict-origin-when-cross-origin'
+        }
+        for key, value in headers.items():
+            self.assertEqual(response.headers.get(key), value)
 
     def test_cors_security(self):
         """It should return a CORS header"""
@@ -201,4 +202,3 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # Check for the CORS header
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
-        
